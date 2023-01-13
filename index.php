1d06eb90d835aa0f8c2d369a8ea6ad949db30946
Jane246 <janejennie042@gmail.com>
2023-01-13 14:02:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        What is your name:<input type = "text" name = "name"><br>
+        What is your age:<input type = "text" name = "age"><br>
+        What is your gender: <br>
+        <input type="radio" name="gender" value="male"> male<br>
+        <input type="radio" name="gender" value="female"> female<br>
+        What is your marrital status:<br>
+        <input type = "radio" name = "marrital_status" value="single"> single<br>
+        <input type = "radio" name = "marrital_status" value="married"> married<br>
+        <input type = "radio" name = "marrital_status" value="divorced"> divorced<br>
+        <input type = "radio" name = "marrital_status" value="widowed"> widowed<br>
+        <input type = "submit" value = "sumit">
+        
+    </form>
+    
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+        $name = $_GET["name"];
+        $age = $_GET["age"];
+        $gender = $_GET["gender"];
+        $marrital_status = $_GET["marrital_status"];
+        $name_title = "";
+        $male = "male";
+        $female = "female";
+        $married = "married";
+        $widowed = "widowed";
+        
+        //echo " ".$name_titie."<br>";
+        if ($gender == $male)
+        {
+            if ($age <= 14)
+            {
+                $name_title = "ด.ช";
+            }
+            else
+            {
+                $name_title = "นาย";
+            }
+        }
+        else if ($gender == $female)
+        {
+            if ($age <= 14)
+            {
+                $name_title = "ด.ญ";
+            }
+            else if ($marrital_status==$married||$marrital_status==$widowed)
+            {
+                $name_title = "นาง";
+            }
+            else
+            {
+                $name_title = "นางสาว";
+            }
+        }
+        echo $name_title. "   ".$name."    ".$age."    ".$gender."    ".$marrital_status;
+    ?>
+</body>
+
+</html>
